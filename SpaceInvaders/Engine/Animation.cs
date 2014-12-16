baf4947f66cf4641c451c840fee0f4ff6400b81c
trox085 <trox085@gmail.com>
2014-12-16 21:48:53 +0000

    Animation instances may now have their frames set after the animation has been created.  Previously the frames could only be set in the constructor.  This was modified due to the order in which Initialize and LoadContent methods are called in the game.  I expected the order to be Game.Initialize, GameComponent.Initialize, Game.LoadContent, GameComponent.LoadContent.  However it executes as Game.Initialize, GameComponent.Initialize, GameComponent.LoadContent, Game.LoadComponent.
    BaseGame is now generic so that the extending game may be referenced directly.  GameState, GameStateManager, TitleScreen, and PlayScreen were modified accordingly to accept the generic BaseGame.
    
    A new Animations class was added to keep track of all animations that will be used by the SpaceInvaders game.
    
    The sprite sheet that will be used with the game was changed to a much better looking set of sprites than I was able to create...
    
    The SmallInvader class was removed in favor of using a single Invader class from which all invader types may derive.

--- SpaceInvaders/Engine/Animation.cs
+++ SpaceInvaders/Engine/Animation.cs
@@ -1,161 +1,188 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
+
 
 
 namespace SpaceInvaders.Engine
 {
-        internal abstract class Animation
+    internal class Animation
+    {
+        private short _MillisecondsFrameHasBeenDisplayed;
+
+
+
+        public Animation(short defaultNumberOfMillisecondsToDisplayEachFrame, IEnumerable<AnimationFrame> frames)
         {
-                private short _MillisecondsFrameHasBeenDisplayed;
+            if (defaultNumberOfMillisecondsToDisplayEachFrame < 1)
+            {
+                throw new ArgumentException("Must be a positive value.", "defaultNumberOfMillisecondsToDisplayEachFrame");
+            }
+            _DefaultNumberOfMillisecondsToDisplayEachFrame = defaultNumberOfMillisecondsToDisplayEachFrame;
 
 
-                
-                protected Animation(short defaultNumberOfMillisecondsToDisplayEachFrame)
-                {
-                        if(defaultNumberOfMillisecondsToDisplayEachFrame < 1)
-                        {
-                                throw new ArgumentException("Must be a positive value.", "defaultNumberOfMillisecondsToDisplayEachFrame");
-                        }
-                        _DefaultNumberOfMillisecondsToDisplayEachFrame = defaultNumberOfMillisecondsToDisplayEachFrame;
+            if (frames != null)
+            {
+                _AnimationFrames = new List<AnimationFrame>(frames);
+            }
 
-                        _MillisecondsFrameHasBeenDisplayed = 0;
-                        _CurrentFrameIndex = 0;
-                }
-
-
-
-                public abstract void Initialize();
-                public abstract void LoadContent(ContentManager contentManager);
-
-
-
-                /// <summary>
-                /// Gets the frames, in sequential order, which create the animation.
-                /// </summary>
-                protected abstract AnimationFrame[] AnimationFrames { get; }
-
-
-
-                /// <summary>
-                /// Gets the number of milliseconds each frame should be
-                /// displayed, unless the frame explicitly states otherwise.
-                /// </summary>
-                protected short DefaultNumberOfMillisecondsToDisplayEachFrame
-                {
-                        get { return _DefaultNumberOfMillisecondsToDisplayEachFrame; }
-                }
-                private short _DefaultNumberOfMillisecondsToDisplayEachFrame;
-
-
-
-                /// <summary>
-                /// Gets or sets the index of the selected frame.
-                /// </summary>
-                protected short CurrentFrameIndex
-                {
-                        get { return _CurrentFrameIndex; }
-                        set
-                        {
-                                AnimationFrame[] frames = AnimationFrames;
-                                if(frames == null || frames.Length < 1)
-                                {
-                                        _CurrentFrameIndex = 0;
-                                        return;
-                                }
-
-                                if (value < 0)
-                                {
-                                        _CurrentFrameIndex = Convert.ToInt16(frames.Length - 1);
-                                        return;
-                                }
-                                
-                                // Keep the index between 0 and AnimationFrames.Length
-                                _CurrentFrameIndex = Convert.ToInt16(value % AnimationFrames.Length);
-                        }
-                }
-                private short _CurrentFrameIndex;
-
-
-
-                /// <summary>
-                /// Gets the current AnimationFrame of the Animation.
-                /// </summary>
-                protected AnimationFrame CurrentFrame
-                {
-                        get
-                        {
-                                if(_CurrentFrameIndex < 0)
-                                {
-                                        return null;
-                                }
-
-
-                                AnimationFrame[] frames = AnimationFrames;
-                                if(frames == null || _CurrentFrameIndex >= frames.Length)
-                                {
-                                        return null;
-                                }
-
-                                return frames[_CurrentFrameIndex];
-                        }
-                }
-
-
-
-                protected void IncrementFrame()
-                {
-                        CurrentFrameIndex += 1;
-                        _MillisecondsFrameHasBeenDisplayed = 0;
-                }
-
-
-
-                protected void DecrementFrame()
-                {
-                        CurrentFrameIndex -= 1;
-                        _MillisecondsFrameHasBeenDisplayed = 0;
-                }
-
-
-
-                protected void Reset()
-                {
-                        CurrentFrameIndex = 0;
-                        _MillisecondsFrameHasBeenDisplayed = 0;
-                }
-
-
-
-                public void Update(GameTime gameTime)
-                {
-                        AnimationFrame currentFrame = CurrentFrame;
-                        if(currentFrame == null)
-                        {
-                                IncrementFrame();
-                                return;
-                        }
-
-                        _MillisecondsFrameHasBeenDisplayed = Convert.ToInt16(_MillisecondsFrameHasBeenDisplayed + gameTime.ElapsedGameTime.TotalMilliseconds);
-                        if((currentFrame.MillisecondsToShowFrame != null && _MillisecondsFrameHasBeenDisplayed >= currentFrame.MillisecondsToShowFrame.Value)
-                                || (currentFrame.MillisecondsToShowFrame == null && _MillisecondsFrameHasBeenDisplayed >= _DefaultNumberOfMillisecondsToDisplayEachFrame))
-                        {
-                                IncrementFrame();
-                        }
-                }
-
-
-
-                public void Draw(SpriteBatch spriteBatch, Rectangle bounds, Vector2 origin, float rotation, Vector2 scale, Color tint, SpriteEffects effects, float depth)
-                {
-                        AnimationFrame currentFrame = CurrentFrame;
-                        if(currentFrame == null)
-                        {
-                                return;
-                        }
-
-                        spriteBatch.Draw(currentFrame.Image, null, bounds, currentFrame.SourceRectangle, origin, rotation, scale, tint, effects, depth);
-                }
+            _MillisecondsFrameHasBeenDisplayed = 0;
+            _CurrentFrameIndex = 0;
         }
+
+
+
+        /// <summary>
+        /// Gets the frames, in sequential order, which create the animation.
+        /// </summary>
+        protected IList<AnimationFrame> AnimationFrames
+        {
+            get
+            {
+                if (_AnimationFrames == null)
+                {
+                    _AnimationFrames = new List<AnimationFrame>();
+                }
+
+                return _AnimationFrames;
+            }
+        }
+        private List<AnimationFrame> _AnimationFrames;
+
+
+
+        /// <summary>
+        /// Gets the number of milliseconds each frame should be
+        /// displayed, unless the frame explicitly states otherwise.
+        /// </summary>
+        protected short DefaultNumberOfMillisecondsToDisplayEachFrame
+        {
+            get { return _DefaultNumberOfMillisecondsToDisplayEachFrame; }
+        }
+        private short _DefaultNumberOfMillisecondsToDisplayEachFrame;
+
+
+
+        /// <summary>
+        /// Gets or sets the index of the selected frame.
+        /// </summary>
+        protected short CurrentFrameIndex
+        {
+            get { return _CurrentFrameIndex; }
+            set
+            {
+                IList<AnimationFrame> frames = AnimationFrames;
+                if (frames == null || frames.Count < 1)
+                {
+                    _CurrentFrameIndex = 0;
+                    return;
+                }
+
+                if (value < 0)
+                {
+                    _CurrentFrameIndex = Convert.ToInt16(frames.Count - 1);
+                    return;
+                }
+
+                // Keep the index between 0 and AnimationFrames.Length
+                _CurrentFrameIndex = Convert.ToInt16(value % AnimationFrames.Count);
+            }
+        }
+        private short _CurrentFrameIndex;
+
+
+
+        /// <summary>
+        /// Gets the current AnimationFrame of the Animation.
+        /// </summary>
+        protected AnimationFrame CurrentFrame
+        {
+            get
+            {
+                if (_CurrentFrameIndex < 0)
+                {
+                    return null;
+                }
+
+
+                IList<AnimationFrame> frames = AnimationFrames;
+                if (frames == null || _CurrentFrameIndex >= frames.Count)
+                {
+                    return null;
+                }
+
+                return frames[_CurrentFrameIndex];
+            }
+        }
+
+
+
+        protected void IncrementFrame()
+        {
+            CurrentFrameIndex += 1;
+            _MillisecondsFrameHasBeenDisplayed = 0;
+        }
+
+
+
+        protected void DecrementFrame()
+        {
+            CurrentFrameIndex -= 1;
+            _MillisecondsFrameHasBeenDisplayed = 0;
+        }
+
+
+
+        protected void Reset()
+        {
+            CurrentFrameIndex = 0;
+            _MillisecondsFrameHasBeenDisplayed = 0;
+        }
+
+
+
+        public void Update(GameTime gameTime)
+        {
+            AnimationFrame currentFrame = CurrentFrame;
+            if (currentFrame == null)
+            {
+                IncrementFrame();
+                return;
+            }
+
+            _MillisecondsFrameHasBeenDisplayed = Convert.ToInt16(_MillisecondsFrameHasBeenDisplayed + gameTime.ElapsedGameTime.TotalMilliseconds);
+            if ((currentFrame.MillisecondsToShowFrame != null && _MillisecondsFrameHasBeenDisplayed >= currentFrame.MillisecondsToShowFrame.Value)
+                    || (currentFrame.MillisecondsToShowFrame == null && _MillisecondsFrameHasBeenDisplayed >= _DefaultNumberOfMillisecondsToDisplayEachFrame))
+            {
+                IncrementFrame();
+            }
+        }
+
+
+
+        public void Draw(SpriteBatch spriteBatch, Rectangle bounds, Vector2 origin, float rotation, Vector2 scale, Color tint, SpriteEffects effects, float depth)
+        {
+            AnimationFrame currentFrame = CurrentFrame;
+            if (currentFrame == null)
+            {
+                return;
+            }
+
+            spriteBatch.Draw(currentFrame.Image, null, bounds, currentFrame.SourceRectangle, origin, rotation, scale, tint, effects, depth);
+        }
+
+
+
+        public void Draw(SpriteBatch spriteBatch, Vector2 position, Vector2 origin, float rotation, Vector2 scale, Color tint, SpriteEffects effects, float depth)
+        {
+            AnimationFrame currentFrame = CurrentFrame;
+            if (currentFrame == null)
+            {
+                return;
+            }
+
+            spriteBatch.Draw(currentFrame.Image, position, null, currentFrame.SourceRectangle, origin, rotation, scale, tint, effects, depth);
+        }
+    }
 }
